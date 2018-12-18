da9e6791da0a50840815379f9f7f5b216a1486d4
paul <280478662@qq.com>
2018-12-18 15:26:48 +0800

    first commit

---
+++ designpattern/src/main/java/com/paul/demo/designpattern/App.java
@@ -0,0 +1,13 @@
+package com.paul.demo.designpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
